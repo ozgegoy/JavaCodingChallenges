028664d9b33adf22ffdcc52766759a1860dc0937
ozge.goy <ozgegoy@cloudnesil.com>
2023-01-31 20:09:06 +0300

    SumOdd

---
+++ src/Chapter6/SumOdd.java
@@ -0,0 +1,43 @@
+package Chapter6;
+
+public class SumOdd {
+    public static void main(String[] args) {
+//        System.out.println(isOdd(-1));
+//        System.out.println(isOdd(1));
+//        System.out.println(isOdd(2));
+//        System.out.println(isOdd(3));
+//        System.out.println(sumOdd(1, 6));
+//        sumOdd(1, 11);
+//        sumOdd(-1, 10);
+        sumOdd(10, 10);
+    }
+
+    public static boolean isOdd(int number) {
+        if (number > 0) {
+            if (number % 2 == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static int sumOdd(int start, int end) {
+        if (start >= 0 && end >= 0 && start <= end) {
+            int sum = 0;
+            for (isOdd(start); start <= end; start++) {
+                if (isOdd(start) != false) {
+                    sum += start;
+                    System.out.println("start = " + start);
+                }
+            }
+            System.out.println("toplam = " + sum);
+            return sum;
+        } else {
+            System.out.println("-1");
+            return -1;
+        }
+    }
+}
